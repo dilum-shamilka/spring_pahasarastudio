dd6cff21c73d50e9e528409a3f270a46a711166c
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2026-02-12 19:26:56 +0530

    first commit

---
+++ src/main/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.pahasarastudiospringfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahasarastudiospringfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahasarastudiospringfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.pahasarastudiospringfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahasarastudiospringfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
