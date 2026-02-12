f585d558e144c5286e6141593d3e7ef35cdbdcc8
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2026-02-12 19:49:47 +0530

    changes

--- src/main/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplication.java
+++ src/main/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PahasarastudiospringfinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PahasarastudiospringfinalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PahasarastudiospringfinalApplication.class, args);
+    }
 
 }
--- src/test/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplicationTests.java
+++ src/test/java/lk/ijse/pahasarastudiospringfinal/PahasarastudiospringfinalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PahasarastudiospringfinalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
