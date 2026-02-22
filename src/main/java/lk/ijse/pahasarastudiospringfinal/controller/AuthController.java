c3eda3ae8b8a5e30586623a5698b273b9a9d66d7
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2026-02-22 09:11:30 +0530

    fixed issue

--- src/main/java/lk/ijse/pahasarastudiospringfinal/controller/AuthController.java
+++ src/main/java/lk/ijse/pahasarastudiospringfinal/controller/AuthController.java
@@ -1,12 +1,12 @@
 package lk.ijse.pahasarastudiospringfinal.controller;
 
-import lk.ijse.pahasarastudiospringfinal.dto.AuthDTO;
-import lk.ijse.pahasarastudiospringfinal.dto.ResponseDTO;
+import lk.ijse.pahasarastudiospringfinal.dto.*;
 import lk.ijse.pahasarastudiospringfinal.entity.User;
-import lk.ijse.pahasarastudiospringfinal.repo.UserRepo;
+import lk.ijse.pahasarastudiospringfinal.service.UserService;
+import lk.ijse.pahasarastudiospringfinal.util.JwtUtil;
+import lk.ijse.pahasarastudiospringfinal.util.MappingUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,23 +16,37 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private MappingUtil mappingUtil;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<ResponseDTO> login(@RequestBody AuthDTO authDTO) {
-        return userRepo.findAll().stream()
-                .filter(u -> u.getEmail().equals(authDTO.getEmail()))
-                .findFirst()
-                .map(user -> {
-                    if (passwordEncoder.matches(authDTO.getPassword(), user.getPassword())) {
-                        return ResponseEntity.ok(new ResponseDTO("00", "Success", user.getRole().name()));
-                    }
-                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResponseDTO("01", "Wrong Password", null));
-                })
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO("01", "User Not Found", null)));
+        try {
+            // Fetch actual User entity from DB
+            User user = userService.loadUserEntityByEmail(authDTO.getEmail());
+
+            if (!passwordEncoder.matches(authDTO.getPassword(), user.getPassword())) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(new ResponseDTO("01", "Authentication Failed: Wrong Password", null));
+            }
+
+            UserDTO userDTO = mappingUtil.toUserDTO(user);
+            String token = jwtUtil.generateToken(userDTO);
+
+            return ResponseEntity.ok(new ResponseDTO("00", "Login Successful", token));
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseDTO("01", e.getMessage(), null));
+        }
     }
 
     @PostMapping("/logout")
